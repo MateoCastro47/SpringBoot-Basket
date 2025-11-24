ce33250b7255bd1e360e4f2d445174d16d0099a9
MateoCastro47 <castrosoutomateo@gmail.com>
2025-11-24 12:34:33 +0100

    Añado funcion para verificar nombre único en liga

--- src/main/java/com/edu/back/mcs/PracticaFinal/repository/EquipoRepository.java
+++ src/main/java/com/edu/back/mcs/PracticaFinal/repository/EquipoRepository.java
@@ -1,10 +1,11 @@
 package com.edu.back.mcs.PracticaFinal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edu.back.mcs.PracticaFinal.model.Equipo;
 
-public interface EquipoRepository extends JpaRepository<Equipo, Long> {
-
-
+public interface EquipoRepository extends JpaRepository<Equipo, Long>{
+    Optional<Equipo> findByNombreAndLigaId(String nombre, Long Liga_id);
 }
