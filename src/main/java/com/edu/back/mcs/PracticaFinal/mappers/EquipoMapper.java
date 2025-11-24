2a4039c18437f126b36bcb58cc6275bb6bee5ff0
MateoCastro47 <castrosoutomateo@gmail.com>
2025-11-24 12:28:25 +0100

    Creado el mapper para Equipo

---
+++ src/main/java/com/edu/back/mcs/PracticaFinal/mappers/EquipoMapper.java
@@ -0,0 +1,20 @@
+package com.edu.back.mcs.PracticaFinal.mappers;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import com.edu.back.mcs.PracticaFinal.model.Equipo;
+import com.edu.back.mcs.PracticaFinal.model.DTO.EquipoDetalleDTO;
+
+@Mapper(componentModel = "spring")
+public interface EquipoMapper {
+    EquipoDetalleDTO toDTO(Equipo equipo);
+
+    Equipo toEntity(EquipoDetalleDTO dto);
+
+    List<EquipoDetalleDTO> toDTOList(List<Equipo> equipos);
+
+    void updateEntityFromDTO(EquipoDetalleDTO dto, @MappingTarget Equipo equipo);
+}
