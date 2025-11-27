b977d3c1e95ef8e033c9a28f8a6c1b793bd413fb
MateoCastro47 <castrosoutomateo@gmail.com>
2025-11-27 12:08:52 +0100

    Modificado mapper de Entrenador

--- src/main/java/com/edu/back/mcs/PracticaFinal/mappers/EntrenadorMapper.java
+++ src/main/java/com/edu/back/mcs/PracticaFinal/mappers/EntrenadorMapper.java
@@ -3,19 +3,20 @@ package com.edu.back.mcs.PracticaFinal.mappers;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-import com.edu.back.mcs.PracticaFinal.model.DTO.EntrenadorResumenDTO;
+import com.edu.back.mcs.PracticaFinal.model.DTO.EntrenadorDetalleDTO;
 import com.edu.back.mcs.PracticaFinal.model.Entrenador;
 
 @Mapper(componentModel = "spring")
 public interface EntrenadorMapper {
-    
-    //Mapear entrenador_id (Entity) → id (DTO)
-    @Mapping(source = "entrenador_id", target="id")
-    @Mapping(target = "equiposEntrenados", ignore = true)
-    EntrenadorResumenDTO toResumenDTO(Entrenador entrenador);
-    
-    List<EntrenadorResumenDTO> toResumenDTOList(List<Entrenador> entrenadores);
+    EntrenadorDetalleDTO tDTO(Entrenador entrenador);
+
+    Entrenador toEntity(EntrenadorDetalleDTO dto);
+
+    List<EntrenadorDetalleDTO> toDTOlist(List<Entrenador> entrenadores);
+
+   
+    void updateEntityFromDTO(EntrenadorDetalleDTO dto, @MappingTarget Entrenador entrenador);
 }
 
