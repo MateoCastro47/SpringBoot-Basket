351695d13bbf2a10367961130e10623bb4c20f81
MateoCastro47 <castrosoutomateo@gmail.com>
2025-11-27 13:02:23 +0100

    Modificado mapper y repositorio de Liga

--- src/main/java/com/edu/back/mcs/PracticaFinal/mappers/LigaMapper.java
+++ src/main/java/com/edu/back/mcs/PracticaFinal/mappers/LigaMapper.java
@@ -3,14 +3,19 @@ package com.edu.back.mcs.PracticaFinal.mappers;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
-import com.edu.back.mcs.PracticaFinal.model.DTO.LigaResumenDTO;
+import com.edu.back.mcs.PracticaFinal.model.DTO.LigaDetalleDTO;
 import com.edu.back.mcs.PracticaFinal.model.Liga;
 
 @Mapper(componentModel = "spring")
 public interface LigaMapper {
     
-    LigaResumenDTO toResumenDTO(Liga liga);
-    
-    List<LigaResumenDTO> toResumenDTOList(List<Liga> ligas);
+    LigaDetalleDTO toDTO(Liga liga);
+
+    Liga toEntity(LigaDetalleDTO dto);
+
+    List<LigaDetalleDTO> toDTOlist(List<Liga> ligas);
+
+    void updateEntityFromDTO(LigaDetalleDTO dto, @MappingTarget Liga liga);
 }
--- src/main/java/com/edu/back/mcs/PracticaFinal/repository/LigaRepository.java
+++ src/main/java/com/edu/back/mcs/PracticaFinal/repository/LigaRepository.java
@@ -1,9 +1,16 @@
 package com.edu.back.mcs.PracticaFinal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edu.back.mcs.PracticaFinal.model.Liga;
 
-public interface LigaRepository extends JpaRepository<Liga, Long>{
-    
+public interface LigaRepository extends JpaRepository<Liga, Long> {
+
+    // Verificar si existe una liga con el mismo nombre en el mismo pais
+    boolean existsByNombreAndPais(String nombre, String pais);
+
+    // Buscar liga por nombre y pais (para Actualización)
+    Optional<Liga> findByNombreAndPais(String nombre, String pais);
 }
