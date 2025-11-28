b4b3c89715e0248a9c53a313f8b702bb31796f57
MateoCastro47 <castrosoutomateo@gmail.com>
2025-11-28 10:17:07 +0100

    Modificados repositorios de Fichaje y Partido

--- src/main/java/com/edu/back/mcs/PracticaFinal/repository/FichajesRepository.java
+++ src/main/java/com/edu/back/mcs/PracticaFinal/repository/FichajesRepository.java
@@ -1,10 +1,26 @@
 package com.edu.back.mcs.PracticaFinal.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.edu.back.mcs.PracticaFinal.model.Fichaje;
+import com.edu.back.mcs.PracticaFinal.model.Jugador;
+import com.edu.back.mcs.PracticaFinal.model.ValueObjects.Temporada;
+
 
 
 public interface FichajesRepository extends JpaRepository<Fichaje, Long>{
+
+    List<Fichaje> findByJugadorAndFechaFin(Jugador jugador, LocalDate fechaFin);
     
+    List<Fichaje> findByJugador(Jugador jugador);
+
+    List<Fichaje> findByTemporada(Temporada temporada);
+
+    @Query("SELECT f FROM Fichaje f WHERE f.fechaFin > :fecha")
+    List<Fichaje> findFichajesActivos(@Param("fecha") LocalDate fecha);
 }
--- src/main/java/com/edu/back/mcs/PracticaFinal/repository/PartidoRepository.java
+++ src/main/java/com/edu/back/mcs/PracticaFinal/repository/PartidoRepository.java
@@ -1,9 +1,23 @@
 package com.edu.back.mcs.PracticaFinal.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.edu.back.mcs.PracticaFinal.model.Equipo;
 import com.edu.back.mcs.PracticaFinal.model.Partido;
 
 public interface PartidoRepository extends JpaRepository<Partido, Long>{
     
+    boolean existsByEquipoLocalAndEquipoVisitanteAndFecha(
+        Equipo equipoLocal,
+        Equipo equipoVisitante,
+        LocalDate fechaPartido
+    );
+
+    Optional<Partido> findByEquipoLocalAndEquipoVisitanteAndFecha(
+        Equipo equipoLocal,
+        Equipo equipoVisitante,
+        LocalDate fechaPartido
+    );
 }
